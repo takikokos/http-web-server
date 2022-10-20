a26e7f480f9b3301bb5ebb27dac1cda87b9a79ea
Mikhail Abrash <bastazf@mail.ru>
2022-10-20 13:02:25 +0300

    added uploading files

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
-from flask import Flask
+from hashlib import sha256
+from pathlib import Path
+
+from flask import Flask, request
 from flask_httpauth import HTTPBasicAuth
-from werkzeug.security import generate_password_hash, check_password_hash  
+from werkzeug.security import generate_password_hash, check_password_hash # use Flask-Bcrypt?
 
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = Path('store')
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1000 * 1000 # 16 Mb
+
 auth = HTTPBasicAuth()
 
 users = {
     "test": generate_password_hash("pass"),
 }
+
 
 @auth.verify_password
 def verify_password(username, password):
@@ -20,18 +27,30 @@ def verify_password(username, password):
 @app.route('/', methods = ['GET'])
 def get_file():
     return {'data': 'get'}
-  
+
 
 @app.route('/', methods = ['POST'])
 @auth.login_required
 def upload_file():
-    return {'data': 'post'}
+    if 'file' not in request.files:
+        return "No file part in post request", 400
+    file = request.files['file']
+
+    if file.filename == '':
+        return 'No selected file', 400
+
+    filename_hash = sha256(file.filename.encode()).hexdigest()
+    filename = app.config['UPLOAD_FOLDER'] / filename_hash[:2] / filename_hash
+    filename.parent.mkdir(parents=True, exist_ok=True)
+    file.save(filename)
+
+    return filename_hash
 
 
 @app.route('/', methods = ['DELETE'])
 def delete_file():
     return {'data': 'DELETE'}
-  
-  
+
+
 if __name__ == '__main__':
     app.run(debug=True)
--- test_crud.sh
+++ test_crud.sh
@@ -5,23 +5,15 @@ curl http://localhost:5000/
 echo
 
 echo "Testing POST http://localhost:5000/ without auth"
-curl -X POST http://localhost:5000/ \
-   -H "Content-Type: application/json"\
-   -d '{"foo": "bar"}'
+curl -F file=@uploadme.txt http://localhost:5000/
 echo
 
 echo "Testing POST http://localhost:5000/ with auth"
-curl -X POST http://localhost:5000/ \
-   -H "Content-Type: application/json"\
-   -d '{"foo": "bar"}'\
-   -u "test:pass"
+curl -F file=@uploadme.txt http://localhost:5000/ -u "test:pass"
 echo
 
 echo "Testing POST http://localhost:5000/ with wrong auth"
-curl -X POST http://localhost:5000/ \
-   -H "Content-Type: application/json"\
-   -d '{"foo": "bar"}'\
-   -u "wrong:password"
+curl -F file=@uploadme.txt http://localhost:5000/ -u "wrong:password"
 echo
 
 echo "Testing DELETE http://localhost:5000/ "
